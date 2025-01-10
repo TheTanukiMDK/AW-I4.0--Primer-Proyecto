a34ca3bf5c23fec845568a086eba19ffd9849a1d
SnowyMiau <riquicraft04@gmail.com>
2025-01-10 17:37:43 -0500

    Inicio de sesion y registro terminado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import '../styles/Login.css'
 
 function Login() {
     return (
-        <div>
-            <h1>Este es el login</h1>
+        <div className='wrapper'>
+            <form action="">
+                <h1>Iniciar sesion</h1>
+                <div className='input-box'>
+                    <input type='text' placeholder='Correo electronico'></input>
+                </div>
+                <div className='input-box'>
+                    <input type='password' placeholder='Contraseña'></input>
+                </div>
+                <button type='submit'>Ingresar</button>
+
+                <div className='register-link'>
+                    <p>¿No tienes cuenta? <a href='/Registro'>Registrate</a></p>
+                </div>
+            </form>
         </div>
     )
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import '../styles/Register.css'
+
 
 function Register() {
     return (
-        <div>
+        <div className='wrapper'>
+            <form action="">
+                <h1>Registro</h1>
+                <div className='input-box'>
+                    <input type='text' placeholder='Ingresa correo electronico'></input>
+                </div>
+                <div className='input-box'>
+                    <input type='password' placeholder='Ingresa contraseña'></input>
+                </div>
+                <div className='input-box'>
+                    <input type='password' placeholder='Confirmar contraseña'></input>
+                </div>
+                <button type='submit'>Registrar</button>
 
+                <div className='register-link'>
+                    <p>¿Ya tienes cuenta? <a href='/'>Inicia sesion</a></p>
+                </div>
+            </form>
         </div>
     )
 }

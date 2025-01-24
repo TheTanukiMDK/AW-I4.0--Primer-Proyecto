06134beef5315537370545264a6363acc8f6a8c0
SnowyMiau <riquicraft04@gmail.com>
2025-01-24 18:23:31 -0500

    Rutas protegidas desactivadas, en desarrollo

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -3,12 +3,12 @@ import { Navigate } from "react-router-dom"; // Si usas react-router-dom
 // Usa `useRouter` y lógica de redirección si usas Next.js
 
 const ProtectedRoute = ({ children }) => {
-    const token = localStorage.getItem("token"); // Verifica si el token existe
+   // const token = localStorage.getItem("token"); // Verifica si el token existe
 
-    if (!token) {
+  //  if (!token) {
         // Si no hay token, redirige al inicio de sesión
-        return <Navigate to="/" replace />;
-    }
+       // return <Navigate to="/" replace />;
+   // }
 
     // Si hay token, renderiza el contenido protegido
     return children;
